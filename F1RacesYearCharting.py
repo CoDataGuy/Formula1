698b27442d96adbd748ea87534d8039ec6ca276c
Shannon <shannon.od3.dev@gmail.com>
2026-01-02 09:50:08 -0700

    Delete F1RacesYearCharting.py
    Incomplete file removed from public repository

--- F1RacesYearCharting.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-# Best “happy path” import
-df = pd.read_csv("f1RacesYear.csv")  # or r"C:\path\to\my_file.csv" on Windows
-
-# Quick sanity check
-print(df.head())
-print(df.info())
